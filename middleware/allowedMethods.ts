d1aea48be5a204a7da6637e803063c8b7ecc1753
Wasiu Idowu <hoxtygen@live.com>
2023-02-20 08:35:41 +0100

    fix: await next action
    when a request is made it returns the result and a warning that says
    "API resolved without sending a response for <insert route>,
    this may result in stalled requests."
    
    This happens because the await keyword isn't used to invoke
    the next() action in a middleware used to check the REST method
    used on a given route.
    By adding await, the warning no longer show up.

--- middleware/allowedMethods.ts
+++ middleware/allowedMethods.ts
@@ -1,12 +1,11 @@
-import { Middleware} from 'next-api-route-middleware';
-
+import { Middleware } from "next-api-route-middleware";
 
 export const allowMethods = (allowedMethods: string[]): Middleware => {
-    return async function (req, res, next) {
-      if (allowedMethods.includes(req.method!) || req.method == 'OPTIONS') {
-        next();
-      } else {
-        res.status(405).send({ message: 'Method not allowed.' });
-      }
-    };
+  return async function (req, res, next) {
+    if (allowedMethods.includes(req.method!) || req.method == "OPTIONS") {
+      await next();
+    } else {
+      res.status(405).json({ status: 405, message: "Method not allowed." });
+    }
   };
+};
