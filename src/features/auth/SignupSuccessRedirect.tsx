6e313e01abff4a1ae001a6297ae10dc99ee67fdb
Wasiu Idowu <hoxtygen@live.com>
2024-06-20 09:13:03 +0100

    chore: add redirect page
    A redirect page where newly registered users are redirected
    was added. When registration is successful, users are
    notified and shown the next step of the registration process
    on this page.

---
+++ src/features/auth/SignupSuccessRedirect.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SignupSuccessRedirect() {
+  return (
+    <div>
+      <div className="mt-14">
+        <h2 className="text-32 font-hanken-medium text-white">
+          Registration Successful!.
+        </h2>
+        <h3 className="text-24 font-hanken-medium text-white">
+          A verification link have been sent to your email.
+        </h3>
+      </div>
+    </div>
+  );
+}
