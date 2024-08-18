9d47ffdf3adf6639b7e8159f62bb73c76294368b
Wasiu Idowu <hoxtygen@live.com>
2024-08-18 09:08:40 +0100

    chore: rename directory
    The directory was renamed as it was conflicting with
    a global module also named constant.

---
+++ src/constant-data/index.ts
@@ -0,0 +1,29 @@
+export {
+  buildingPurpose,
+  buildingType,
+  facilityQuality,
+  petsAllowed,
+  apartmentType,
+  apartmentDetails,
+  buildingTypeToBuy,
+  salutation,
+  employmentType,
+} from "./staticData";
+export { default as mockData } from "./MOCK_DATA.json";
+export { default as homeInspirationData } from "./homeInspData.json";
+export const randomImages = [
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1700390651/home360/zyghhgwoujtwotfqk2hf.jpg",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1700390636/home360/bbpctr5gglqlxgutoyyi.jpg",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1693683559/home360/zomkajforqzizn5eda1f.webp",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1551327496/vlcsnap-2013-04-25-21h12m16s151_hljygd.png",
+
+  "https://www.immobilienscout24.de/content/dam/is24/anbieten/bilder/wissen-hub/banner-visual-01x2.jpg",
+  "https://www.immobilienscout24.de/content/dam/is24/anbieten/bilder/wissen-hub/banner-visual-01x22.jpg",
+  "https://www.immobilienscout24.de/content/dam/is24/anbieten/bilder/wissen-hub/banner-visual-02x2.jpg",
+  "https://www.immobilienscout24.de/content/dam/is24/anbieten/bilder/wissen-hub/banner-visual-04x2.jpg",
+  "https://www.immobilienscout24.de/content/dam/is24/anbieten/bilder/wissen-hub/banner-visual-05x2.jpg",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1700390651/home360/zyghhgwoujtwotfqk2hf.jpg",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1700390636/home360/bbpctr5gglqlxgutoyyi.jpg",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1693683559/home360/zomkajforqzizn5eda1f.webp",
+  "https://res.cloudinary.com/dh3jxarvg/image/upload/v1551327496/vlcsnap-2013-04-25-21h12m16s151_hljygd.png",
+];
