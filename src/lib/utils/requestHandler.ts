0433d5be6da907830f33374d68229268bf4fdc1b
Wasiu Idowu <hoxtygen@live.com>
2024-12-06 09:50:38 +0100

    chore: modify requesthandler
    The request handler function was modified to be type-safe

--- src/lib/utils/requestHandler.ts
+++ src/lib/utils/requestHandler.ts
@@ -1,6 +1,10 @@
+import { AxiosRequestConfig } from "axios";
 import axiosClient from "./axiosInstance";
 
-export default async function requestHandler<T>(url: string, options?: {}) {
+export default async function requestHandler<T>(
+  url: string,
+  options: AxiosRequestConfig = {}
+) {
   const response = await axiosClient<T>(url, { ...options });
   return response;
 }
