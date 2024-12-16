a62276048256c8c73d915c95e3324e9f91b67e1f
Wasiu Idowu <hoxtygen@live.com>
2024-12-16 22:39:47 +0100

    feat: integrate post views endpoint
    As user's view each listing they're recorded in the database.
    This helps in analyzing the performance of each listing and how
    desirable each listing is.

---
+++ src/hooks/usePostListingView.ts
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+import { HOME_360_POST_LISTING_VIEW } from "lib/endpoints";
+import requestHandler from "lib/utils/requestHandler";
+
+type ListingViewParams = {
+  listingId: string;
+  timestamp: string;
+};
+
+export default function usePostListingView() {
+  const { data, status, error, mutate } = useMutation({
+    mutationKey: ["post_listing_view"],
+    mutationFn: ({ listingId, timestamp }: ListingViewParams) =>
+      requestHandler(HOME_360_POST_LISTING_VIEW, {
+        method: "POST",
+        data: {
+          listingId,
+          timestamp,
+        },
+      }),
+    networkMode: "online",
+  });
+  return {
+    mutateListingView: mutate,
+  };
+}
